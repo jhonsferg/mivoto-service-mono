c0a4c953fe0677284c1e9890e9569c4f0f88a2fe
jhonsferg <jhon.fernandez.sg@gmail.com>
2026-01-15 08:22:44 -0500

    Repository initialized

---
+++ src/main/java/pe/com/mivoto/service/MivotoServiceMonoApplication.java
@@ -0,0 +1,13 @@
+package pe.com.mivoto.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MivotoServiceMonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MivotoServiceMonoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/com/mivoto/service/MivotoServiceMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.mivoto.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MivotoServiceMonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
