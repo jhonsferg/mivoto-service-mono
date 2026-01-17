70ea8f6a12464fa75fd0c9384d216ac92cb0ea94
jhonsferg <jhon.fernandez.sg@gmail.com>
2026-01-17 13:51:44 -0500

    Updated application

--- src/main/java/pe/com/mivoto/service/MivotoServiceMonoApplication.java
+++ src/main/java/pe/com/mivoto/service/MivotoServiceMonoApplication.java
@@ -1,13 +1,30 @@
 package pe.com.mivoto.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableCaching
+@EnableAsync
+@EnableScheduling
 public class MivotoServiceMonoApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(MivotoServiceMonoApplication.class, args);
-    }
+        log.info("=".repeat(80));
+        log.info("Iniciando Sistema de Votación Electrónica");
+        log.info("=".repeat(80));
 
+        SpringApplication.run(MivotoServiceMonoApplication.class, args);
+
+        log.info("=".repeat(80));
+        log.info("Sistema de Votación Electrónica iniciado correctamente");
+        log.info("Documentación API disponible en: http://localhost:8080/swagger-ui.html");
+        log.info("=".repeat(80));
+    }
 }
